ce6387b4d01825931d424d34ae2fc4de52c14e15
Yasser Tahiri <yasserth19@gmail.com>
2021-04-05 02:09:03 +0100

    Update

--- ptf.py
+++ ptf.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
-# CODE BY: ARI (3xploit)
-# EMAIL: xzhack206@gmail.com
 #
 """
-PTF {Pentest Tools Framework}
+PXXTF {Pentest Tools Framework}
 """
 from core.excute import main
 if __name__ == "__main__":
